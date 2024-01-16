088b3e92feca264ed55214b93529f06fa79c4726
sora <133684349+sorawaslocked@users.noreply.github.com>
2024-01-16 13:12:45 +0600

    Add computing function when equal is pressed

--- main.js
+++ main.js
@@ -2,39 +2,58 @@ const digitBtns = Array.from(document.querySelectorAll('.digit'));
 const operatorBtns = Array.from(document.querySelectorAll('.operator'));
 const equalBtn = document.querySelector('#equal');
 const display = document.querySelector('#display');
-const equalAndOperators = operatorBtns.slice().push(equalBtn);
+const equalAndOperators = operatorBtns.slice();
+equalAndOperators.push(equalBtn);
 let leftNumber = "";
 let rightNumber = "";
 let operator = "";
 
 function changeDisplay() {
-  display.textContent = leftNumber + operator + rightNumber;
+  if (display.textContent.length < 16)
+    display.textContent = leftNumber + operator + rightNumber;
 }
 
-digitBtns.forEach((btn) => {
+function operate(a, b, operator) {
+  switch (operator) {
+    case '+':
+      return a + b;
+    case '−':
+      return a - b;
+    case '×':
+      return a * b;
+    case '÷':
+      return a / b;
+  }
+}
+
+digitBtns.forEach(btn => {
   btn.addEventListener('click', () => {
-    if (display.textContent.length <= 16) {
-      if (operator === "") {
-        leftNumber += btn.textContent;
-      }
-      else {
-        rightNumber += btn.textContent;
-      }
+    if (operator === "") {
+      leftNumber += btn.textContent;
+    }
+    else {
+      rightNumber += btn.textContent;
     }
     changeDisplay();
   });
 });
 
-operatorBtns.forEach((btn) => {
+operatorBtns.forEach(btn => {
   btn.addEventListener('click', () => {
-    operator = btn.textContent;
-    changeDisplay();
+    if (leftNumber !== "") {
+      operator = btn.textContent;
+      changeDisplay();
+    }
   });
 });
 
-operate = {
-  "+": (a, b) => a + b,
-  "−": (a, b) => a - b,
-  "×": (a, b) => a * b,
-  "÷": (a, b) => a / b,
-}
+equalAndOperators.forEach(btn => {
+  btn.addEventListener('click', () => {
+    if (leftNumber !== "" && rightNumber !== "") {
+      leftNumber = operate(leftNumber, rightNumber, operator);
+      operator = "";
+      rightNumber = "";  
+      changeDisplay();
+    }
+  });
+});
